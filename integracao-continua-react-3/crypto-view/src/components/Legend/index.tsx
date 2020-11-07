d34de45ae9fd81676f6621ba85fc70d70b9437df
Rômulo Barbosa Ribeiro <romulo.ribeiro@hbsis.com.br>
2020-11-07 17:52:58 -0300

    integracao-continua

---
+++ integracao-continua-react-3/crypto-view/src/components/Legend/index.tsx
@@ -0,0 +1,17 @@
+// @flow
+import * as React from 'react';
+import './index.css';
+
+interface LegendProps {
+    legend: string
+};
+
+export const Legend: React.FC<LegendProps> = (props) =>
+{
+    const {legend} = props;
+    return (
+        <div className="Legend">
+            {legend}
+        </div>
+    )
+}
